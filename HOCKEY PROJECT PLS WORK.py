9c786ca5e17efba45adc1324faa71d9eab43ee8c
pbalalas <pbalalas@monkswalk.herts.sch.uk>
2025-06-21 06:32:36 +0000

    kdifhbksdcyb sidybv oiudfv

--- HOCKEY PROJECT PLS WORK.py
+++ HOCKEY PROJECT PLS WORK.py
@@ -7,63 +7,63 @@ import random
 
 #Create dictionaries to store the musicians in multiple json files
 artists = {
-    "lewiscapaldi": {
+    "Lewis Capaldi": {
         "Someone You Loved": "someoneyouloved.mp3",
         "Before You Go": "beforeyougo.mp3",
         "Hold Me While You Wait": "holdmewhileyouwait.mp3",
         "Forever You Go": "foreveryougo.mp3",
         "Last Goodbye": "lastgoodbye.mp3"
     },
-    "greenday": {
+    "Greenday": {
         "Basket Case": "https://raw.githubusercontent.com/pbalalas/Audio-Python/main/Basket%20Case.mp3",
         "American Idiot": "https://raw.githubusercontent.com/pbalalas/Audio-Python/main/Green%20Day%20-%20American%20Idiot%20Official%20Audio%20(1).mp3",
         "Good Riddance": "https://raw.githubusercontent.com/pbalalas/Audio-Python/main/Green%20Day%20-%20Good%20Riddance%20Time%20of%20Your%20Life%20Official%20Music%20Video%204K%20UPGRADE.mp3",
-        "When I Come Around": "whenicomearound.mp3",
-        "Boulevard of Broken Dreams": "boulevardofbrokendreams.mp3"
+        "When I Come Around": "https://raw.githubusercontent.com/pbalalas/Audio-Python/main/When%20I%20Come%20Around.mp3",
+        "Boulevard of Broken Dreams": "https://raw.githubusercontent.com/pbalalas/Audio-Python/main/Green%20Day%20-%20Boulevard%20of%20Broken%20Dreams%20Official%20Audio.mp3"
     },
-    "foofighters": {
+    "Foo Fighters": {
         "Everlong": "everlong.mp3",
         "Wheels": "wheels.mp3",
         "Learn to Fly": "learntofly.mp3",
         "Monkey Wrench": "monkeywrench.mp3",
         "Big Me": "bigme.mp3"
     },
-    "shawnmendes": {
+    "Shawn Mendes": {
         "Stitches": "stitches.mp3",
         "In My Blood": "inmyblood.mp3",
         "Senorita": "senorita.mp3",
         "Mercy": "mercy.mp3",
         "Lost in Japan": "lostinjapan.mp3"
     },
-    "metallica": {
+    "Metallica": {
         "Nothing Else Matters": "nothingelsematters.mp3",
         "Enter Sandman": "entersandman.mp3",
         "One Battery": "onebattery.mp3",
         "Sad But True": "sadbuttrue.mp3",
         "The Unforgiven": "theunforgiven.mp3"
     },
-    "beatles": {
+    "Beatles": {
         "Hey Jude": "heyjude.mp3",
         "Let Me": "letme.mp3",
         "Come In": "comein.mp3",
         "You Never Alone": "youneveralone.mp3",
         "All I’ve Made Love": "allivemadelove.mp3"
     },
-    "mozart": {
+    "Mozart": {
         "Symphony No. 40 in G minor, K. 550": "symphonyno40ingminor,k.550.mp3",
         "The Requiem": "therequiem.mp3",
         "Eine kleine Nachtmusik": "einekleinenachtmusik.mp3",
         "Piano Sonata No. 16 in C major, K. 545": "pianosonatano16incmajor,k.545.mp3",
         "Don Giovanni": "dongiovanni.mp3"
     },
-    "arcticmonkeys": {
+    "Arctic Monkeys": {
         "Do I Wanna Know?": "doiwannaknow?.mp3",
         "R U Mine?": "rumine?.mp3",
         "Arabella": "arabella.mp3",
         "Cornerstone": "cornerstone.mp3",
         "Fluorescent Adolescent": "fluorescentadolescent.mp3"
     },
-    "bach": {
+    "Bach": {
         "Toccata and Fugue in D minor, BWV 565": "toccataandfugue.mp3",
         "Brandenburg Concerto No. 3": "brandenburgconcerto.mp3",
         "Air on the G String": "aironagstring.mp3",
@@ -136,12 +136,12 @@ def login():
     password = str(input("enter your password:   "))
     if userLogin[username]["password"] == password:
         print("login successful")
-        return "successful"
+        return "successful", username
     else:
         while userLogin[username]["password"] != password:
             password = str(input("password is incorrect, try again:   "))
         print("login successful")
-        return "successful"
+        return "successful", username
     
 #create user interface
 #ask user to login or sign up
@@ -163,11 +163,13 @@ while True:
         elif result == "successful":
             break
     elif login_flag != -1:
-        result = login()
+        result, username = login()
         if result == "signup":
             signUp_flag = 0
             login_flag = -1
         elif result == "successful":
+            userLogin = readFile("userLogin")
+            print("\nYour score is ", userLogin[username]["score"])
             break
 
 #create function to choose a random url from the dictionary
@@ -175,8 +177,8 @@ def randomSong():
     #chooses a random artist from the dictionary artists
     name = random.choice(list(artists.keys()))
     #chooses a random song from the artist
-    song = random.choice(list(artists[name].keys()))
-    return name, song
+    songName = random.choice(list(artists[name]))
+    return name, songName
 
 #create another function to download the youtube url as audio
 def playAudio(url, duration):    
@@ -202,4 +204,25 @@ def playAudio(url, duration):
     except Exception as e:
         print("there was an error:   {e}")
 
-playAudio("https://raw.githubusercontent.com/pbalalas/Audio-Python/main/Green%20Day%20-%20American%20Idiot%20Official%20Audio%20(1).mp3", 5)
+#start creating UI to guess the song
+while True: 
+    play_game = input("Do you want to play?   ")
+    if play_game.lower().replace(" ","").find("yes") != -1:
+        break
+    elif play_game.lower().replace(" ", "").find("no") != -1:
+        print("why did you even login??")
+        time.sleep(2)
+        print("what is the point?!?")
+        time.sleep(2)
+        print("what a waste of time!!!!!")
+        time.sleep(2)
+        print("The AMouNt OF FlESh and Bone ThaT wEnT inTo ThiS!!")
+        time.sleep(2)
+        print("YOU WILL REGRET THIS")
+        time.sleep(3)
+        print("goodbye")
+        exit()
+    else:
+        print("please type yes or no")
+        
+print("hello?")
